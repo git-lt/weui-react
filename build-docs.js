8db9aea1bb4f5e9df728477b8e12f7ee5ae4269c
柳涛 <935486956@qq.com>
2017-08-01 10:22:43 +0800

    new docs

--- build-docs.js
+++ build-docs.js
@@ -149,6 +149,13 @@ function buildChangesDoc(changes) {
   for(let v in sortData){
     doc += `\n## ${v}\n`;
     sortData[v].forEach(c=>{
+      c = c.replace(/\[feature\]/, '<span class="change-tag feature">feature</span>')
+        .replace(/\[fix\]/, '<span class="change-tag fix">fix</span>')
+        .replace(/\[new\]/, '<span class="change-tag new">new</span>')
+        .replace(/\[change\]/, '<span class="change-tag change">change</span>')
+        .replace(/\[enhange\]/, '<span class="change-tag enhange">enhange</span>')
+        .replace(/\[deprecated\]/, '<span class="change-tag deprecated">deprecated</span>');
+
       doc += `- ${c}\n`.replace(/(#\d+)/g, '<a href="https://github.com/git-lt/weui-react/issues/$1" target="_bank">$1<a>');
     })
   }
--- docs/changes.md
+++ docs/changes.md
@@ -1,13 +1,13 @@
 
 ## v0.1.15
-- Picker - [feature] 新组件 数据选择器
-- Previewer - [feature] 新组件 图片查看器，点击放大查看图片
+- Picker - <span class="change-tag new">new</span> 新组件 数据选择器
+- Previewer - <span class="change-tag new">new</span> 新组件 图片查看器，点击放大查看图片
 
 ## v0.1.9
-- Header - [feature] 添加Header容器，使用Header Fixed定位
+- Header - <span class="change-tag feature">feature</span> 添加Header容器，使用Header Fixed定位
 
 ## v0.1.8
-- Header - [feature] 添加 `lineColor` 用于配置底边线颜色
+- Header - <span class="change-tag feature">feature</span> 添加 `lineColor` 用于配置底边线颜色
 
 ## v0.1.4
-- Tab - [feature] 添加 `lineColor` 提供指示线颜色的配置
+- Tab - <span class="change-tag feature">feature</span> 添加 `lineColor` 提供指示线颜色的配置
